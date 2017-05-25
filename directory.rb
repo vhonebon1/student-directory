33d103eb698219fa4928f416b830775faf96c6cc
Verity <vhonebon@gmail.com>
2017-05-25 18:51:47 +0100

    Interactive menu further refactored

--- directory.rb
+++ directory.rb
@@ -1,20 +1,51 @@
+@students = []
+
 def input_students
 	puts "Please enter the names of the students"
 	puts "To finish just hit return twice"
-	#create an empty array
-	students = []
 	#get the first name
 	name = gets.chomp
 	#while the name is not empty, repeat this code
 	while !name.empty? do
 		#add the student hash to the array
-		students << {name: name, cohort: :november}
-		puts "Now we have #{students.count} students"
+		@students << {name: name, cohort: :november}
+		puts "Now we have #{@students.count} students"
 		#get another name from the user
 		name = gets.chomp
 	end
-	#return the array of students
-	students
+end
+
+def interactive_menu
+	loop do
+		print_menu
+		process(gets.chomp)
+end
+end
+
+def print_menu
+	puts "1. Input the students"
+	puts "2. Show the students"
+	puts "9. Exit"
+end
+
+
+def show_students
+	print_header
+	print_student_list
+	print_footer
+end
+
+def process(selection)
+case selection
+when "1"
+	input_students
+when "2"
+	show_students
+when "9"
+	exit
+else
+	puts "I don't know what you meant, try again"
+end
 end
 
 def print_header
@@ -22,36 +53,18 @@ def print_header
   puts "-------------"
 end
 
-def print(students)
-  students.each do |student|
+def print_student_list
+  @students.each do |student|
     puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+def print_footer
+  puts "Overall, we have #{@students.count} great students"
 end
 
-def interactive_menu
-	students = []
-	loop do
-		puts "1. Input the students"
-		puts "2. Show the students"
-		puts "9. Exit"
-	selection = gets.chomp
-	case selection
-	when "1"
-		students = input_students
-	when "2"
-		print_header
-		print(students)
-		print_footer(students)
-	when "9"
-		exit
-	else 
-		puts "I don't know what you meant, try again"
-	end
-end
-end
+
 
 interactive_menu
+
+
