fcebd9295fa328bc13b13f3755691758e6edf404
Verity <vhonebon@gmail.com>
2017-05-25 19:34:38 +0100

    Including save function

--- directory.rb
+++ directory.rb
@@ -25,6 +25,7 @@ end
 def print_menu
 	puts "1. Input the students"
 	puts "2. Show the students"
+	puts "3. Save the list to students.csv"
 	puts "9. Exit"
 end
 
@@ -41,6 +42,8 @@ when "1"
 	input_students
 when "2"
 	show_students
+when "3"
+	save_students
 when "9"
 	exit
 else
@@ -63,7 +66,15 @@ def print_footer
   puts "Overall, we have #{@students.count} great students"
 end
 
-
+def save_students
+	file = File.open("students.csv", "w")
+	@students.each do |student|
+		student_data = [student[:name], student[:cohort]]
+		cvs_line = student_data.join(",")
+		file.puts cvs_line
+	end
+		file.close
+end
 
 interactive_menu
 
