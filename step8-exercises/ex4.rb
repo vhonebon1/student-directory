7cc015fa6f4ab23ee28cbc6c8bf348634528f661
Verity <vhonebon@gmail.com>
2017-05-28 14:44:26 +0100

    Solution for exercise 4

---
+++ step8-exercises/ex4.rb
@@ -0,0 +1,34 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+    count = 0
+    while count < students.length
+    puts "#{students[count][:name]} (#{students[count][:cohort]} cohort)"
+    count += 1
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
