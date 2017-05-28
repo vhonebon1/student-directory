0fc27503df6b05a5f1e695021b4ac84ece9e8ca5
Verity <vhonebon@gmail.com>
2017-05-28 14:49:21 +0100

    Solution for exercise 10

---
+++ step8-exercises/ex10.rb
@@ -0,0 +1,32 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.delete!("\n")
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.delete!("\n")
+  end
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
